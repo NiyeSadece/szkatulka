29c42217ae7009c7977a4d80b7564bc338be3db2
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2024-06-26 13:39:28 +0200

    two light on pearls, still weird gaps idc at this point ig

--- try_this.py
+++ try_this.py
@@ -71,37 +71,49 @@ fragment_shader_source_pearl = """
 #version 330 core
 out vec4 FragColor;
 
+in vec3 ourColor;
+in vec2 TexCoord;
 in vec3 FragPos;
 in vec3 Normal;
-in vec3 ourColor;
 
-uniform vec3 lightPos;
+uniform sampler2D ourTexture;
+uniform vec3 lightPos1;
+uniform vec3 lightColor1;
+uniform vec3 lightPos2;
+uniform vec3 lightColor2;
 uniform vec3 viewPos;
-uniform vec3 lightColor;
 
 void main()
 {
     // Ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient1 = ambientStrength * lightColor1;
+    vec3 ambient2 = ambientStrength * lightColor2;
 
     // Diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 lightDir1 = normalize(lightPos1 - FragPos);
+    vec3 lightDir2 = normalize(lightPos2 - FragPos);
+    float diff1 = max(dot(norm, lightDir1), 0.0);
+    float diff2 = max(dot(norm, lightDir2), 0.0);
+    vec3 diffuse1 = diff1 * lightColor1;
+    vec3 diffuse2 = diff2 * lightColor2;
 
     // Specular
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 reflectDir1 = reflect(-lightDir1, norm);
+    vec3 reflectDir2 = reflect(-lightDir2, norm);
+    float spec1 = pow(max(dot(viewDir, reflectDir1), 0.0), 32);
+    float spec2 = pow(max(dot(viewDir, reflectDir2), 0.0), 32);
+    vec3 specular1 = specularStrength * spec1 * lightColor1;
+    vec3 specular2 = specularStrength * spec2 * lightColor2;
 
     // Combine results
-    vec3 result = (ambient + diffuse + specular) * ourColor;
-    FragColor = vec4(result, 1.0);
+    vec3 result = (ambient1 + diffuse1 + specular1 + ambient2 + diffuse2 + specular2) * ourColor;
+    FragColor = texture(ourTexture, TexCoord) * vec4(result, 1.0);
 }
+
 """
 
 angle = 0.0
@@ -443,6 +455,8 @@ def main():
 
     light_pos = glm.vec3(2.0, 2.0, 2.0)
     light_color = glm.vec3(1.0, 1.0, 1.0)
+    light_pos2 = glm.vec3(-1.2, -1.0, -2.0)
+    light_color2 = glm.vec3(0.0, 0.0, 1.0)
     view_pos = glm.vec3(0.0, 0.0, 3.0)
 
 
@@ -496,9 +510,12 @@ def main():
         view_loc = glGetUniformLocation(shader_program_pearl, "view")
         proj_loc = glGetUniformLocation(shader_program_pearl, "projection")
         texture_loc = glGetUniformLocation(shader_program_pearl, "ourTexture")
-        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightPos"), 1, glm.value_ptr(light_pos))
-        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightColor"), 1, glm.value_ptr(light_color))
+        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightPos1"), 1, glm.value_ptr(light_pos))
+        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightColor1"), 1, glm.value_ptr(light_color))
+        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightPos2"), 1, glm.value_ptr(light_pos2))
+        glUniform3fv(glGetUniformLocation(shader_program_pearl, "lightColor2"), 1, glm.value_ptr(light_color2))
         glUniform3fv(glGetUniformLocation(shader_program_pearl, "viewPos"), 1, glm.value_ptr(view_pos))
+
         glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm.value_ptr(view))
         glUniformMatrix4fv(proj_loc, 1, GL_FALSE, glm.value_ptr(projection))
 
